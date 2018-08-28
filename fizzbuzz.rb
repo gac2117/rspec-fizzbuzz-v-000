dbd97893d3e894284284cf47136ba3715749408b
Grace Choi <oiograceoio@gmail.com>
2018-08-28 12:22:31 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,10 @@ def fizzbuzz(int)
     "Fizz"
   elsif int % 5 == 0
     "Buzz"
-  elsif int % 3 == 0 && int  5 == 0
+  elsif int % 3 == 0 && int % 5 == 0
     "Fizzbuzz"
-  else 
-    nil 
+  else
+    nil
   end
 end# Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
